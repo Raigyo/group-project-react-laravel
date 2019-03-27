984d5c8d17e5fbce236f7cce68760e401eab2005
Raigyo <vincent.chilot@gmail.com>
2019-03-27 14:04:21 +0100

    update laravel react

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+    <html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Bonjour</title>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="example"></div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+    </html>
